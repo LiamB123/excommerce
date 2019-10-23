bb8c57b2b3525d16a1be372edd7c3ff16e135436
Ubuntu <ubuntu@ip-172-31-43-192.ec2.internal>
2019-10-23 14:38:24 +0000

    added files

--- search/views.py
+++ search/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from products.models import Product
-# Create your views here.
 
+# Create your views here.
 def do_search(request):
-    products = Product.objects.filter(name_icontains=request.GET['q'])
-    return render(request, "products.html", {'products':products})
+    products = Product.objects.filter(name__icontains=request.GET['q'])
+    return render(request, "products.html", {"products": products})
